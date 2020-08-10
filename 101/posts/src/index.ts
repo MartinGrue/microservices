99a7cf9d54c08ed135baf008809299f7aa5cc678
MartinGrue <gruebler.martin@outlook.de>
2020-08-10 13:45:02 +0200

    init posts

--- 101/posts/src/index.ts
+++ 101/posts/src/index.ts
@@ -1,6 +1,23 @@
-import express from "express";
+import express, { Request } from "express";
+import { randomBytes } from "crypto";
+import bodyparser from "body-parser";
 const app = express();
-app.get("/", (req, res) => {
-  return res.send("Hello from posts");
+app.use(bodyparser.json());
+interface post {
+  id: string;
+  title: string;
+}
+const posts: { [key: string]: post } = {};
+
+app.get("/posts", (req, res) => {
+  return res.send(posts);
 });
+
+app.post("/posts", (req: Request<{}, {}, post>, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = { id, title };
+  return res.status(201).send(posts[id]);
+});
+
 app.listen(2002, () => {});
