5abd0800034d1d2be55c870122965ef39d67cee8
MartinGrue <gruebler.martin@outlook.de>
2020-08-10 13:10:42 +0200

    mode boilerplate

--- 101/comments/src/index.ts
+++ 101/comments/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
 app.get("/", (req, res) => {
-  return res.send("Hello from posts");
+  return res.send("Hello from comments");
 });
-app.listen(2002, () => {});
+app.listen(2001, () => {});
