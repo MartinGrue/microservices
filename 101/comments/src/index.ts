906bd27d98de3bae27f0a24953f12331f12f8732
MartinGrue <gruebler.martin@outlook.de>
2020-08-10 14:49:03 +0200

    Post setup + cors

--- 101/comments/src/index.ts
+++ 101/comments/src/index.ts
@@ -1,6 +1,38 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { randomBytes } from "crypto";
+import bodyparser from "body-parser";
+import cors from "cors";
 const app = express();
-app.get("/", (req, res) => {
-  return res.send("Hello from comments");
-});
+app.use(bodyparser.json());
+app.use(cors());
+
+interface comment {
+  id: string;
+  content: string;
+}
+const commentsByPostId: { [key: string]: comment[] } = {};
+
+app.get(
+  "/posts/:id/comments",
+  (req: Request<{ id: string }, {}, {}>, res: Response<comment[]>) => {
+    const comments = commentsByPostId[req.params.id] || [];
+    return res.send(comments);
+  }
+);
+
+app.post(
+  "/posts/:id/comments",
+  (
+    req: Request<{ id: string }, {}, { content: string }>,
+    res: Response<comment[]>
+  ) => {
+    const commentId = randomBytes(4).toString("hex");
+    const { content } = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
+    return res.status(201).send(comments);
+  }
+);
+
 app.listen(2001, () => {});
--- 101/posts/src/index.ts
+++ 101/posts/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request } from "express";
 import { randomBytes } from "crypto";
 import bodyparser from "body-parser";
+import cors from "cors"
 const app = express();
 app.use(bodyparser.json());
+app.use(cors())
 interface post {
   id: string;
   title: string;
@@ -13,7 +15,7 @@ app.get("/posts", (req, res) => {
   return res.send(posts);
 });
 
-app.post("/posts", (req: Request<{}, {}, post>, res) => {
+app.post("/posts", (req: Request<{}, {}, { title: string }>, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { id, title };
