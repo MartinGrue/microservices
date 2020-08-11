8b58b3a7d478ad0dc2434605ae7c43f3b128f286
MartinGrue <gruebler.martin@outlook.de>
2020-08-11 17:59:31 +0200

    bus stores events now

--- 101/eventbus/src/index.ts
+++ 101/eventbus/src/index.ts
@@ -1,13 +1,15 @@
-import express, { Request } from "express";
+import express, { Request, Response } from "express";
 import bodyparser from "body-parser";
 import axios from "axios";
 import { Event } from "../../shared/Types";
 
 const app = express();
+const events: Event[] = [];
 app.use(bodyparser.json());
 
 app.post("/events", (req: Request<{}, {}, Event>, res) => {
   const event = req.body;
+  events.push(event);
   axios.post<any, any, Event>("http://localhost:4002/events", event);
   axios.post<any, any, Event>("http://localhost:2001/events", event);
   axios.post<any, any, Event>("http://localhost:2002/events", event);
@@ -15,6 +17,9 @@ app.post("/events", (req: Request<{}, {}, Event>, res) => {
 
   console.log("Event fired", event);
   res.send({ status: "OK" });
+});
+app.get("/events", (req, res: Response<Event[]>) => {
+  res.send(events);
 });
 app.listen(4005, () => {
   console.log("bus running on 4005");
--- 101/queryservice/src/index.ts
+++ 101/queryservice/src/index.ts
@@ -7,6 +7,7 @@ import {
   isPostCommentEvent,
   isCommentUpdated,
 } from "../../shared/TypeGuards";
+import axios, { AxiosResponse } from "axios";
 const app = express();
 app.use(bodyparser.json());
 app.use(cors());
@@ -23,28 +24,36 @@ app.get(
     res.send(posts);
   }
 );
-app.post("/events", (req: Request<{}, {}, Event>, res) => {
-  if (isPostPostEvent(req.body)) {
-    const { id, title } = req.body.data;
+const handleEvent = (event: Event) => {
+  if (isPostPostEvent(event)) {
+    const { id, title } = event.data;
     posts[id] = { id, title, comments: [] };
     console.log("new Post");
   }
-  if (isPostCommentEvent(req.body)) {
-    const { id, content, status, postId } = req.body.data;
+  if (isPostCommentEvent(event)) {
+    const { id, content, status, postId } = event.data;
     posts[postId].comments.push({ id, content, status });
     console.log("new Comment");
   }
-  if (isCommentUpdated(req.body)) {
-    const { id, content, status, postId } = req.body.data;
+  if (isCommentUpdated(event)) {
+    const { id, content, status, postId } = event.data;
     const comment = posts[postId].comments.find((c) => c.id === id);
     comment!.status = status;
     comment!.content = content;
     console.log("Comment updated");
   }
-  console.log(posts);
+};
+app.post("/events", (req: Request<{}, {}, Event>, res) => {
+  handleEvent(req.body);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("QueryServive is running on 4002");
+  const events = await axios.get<any, AxiosResponse<Event[]>>(
+    "http://localhsot:4005/events"
+  );
+  events.data.forEach((event) => {
+    handleEvent(event);
+  });
 });
