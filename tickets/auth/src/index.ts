de7ba00e1437f09303ecb1cb612c4c23c063e737
MartinGrue <gruebler.martin@outlook.de>
2020-08-15 14:40:36 +0200

    auth init

---
+++ tickets/auth/src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { json } from "body-parser";
+const app = express();
+app.use(json());
+
+app.listen(4000, () => {
+  console.log("auth listening on 4000");
+});
