3042974bf6c647ac5e4e340dd59c135ea1573311
MartinGrue <gruebler.martin@outlook.de>
2020-08-26 18:39:56 +0200

    added base NATS listerner + createTicket Event typ

---
+++ tickets/verdaccio/src/events/EventTypes.ts
@@ -0,0 +1,14 @@
+import { Subjects } from "./Subjects";
+
+export interface Event {
+  subject: Subjects;
+  data: any;
+}
+export interface TicketCreatedEvent extends Event {
+  subject: Subjects.TicketCreated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+  };
+}
