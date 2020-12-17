2f5ab51f33cd07fe1f6600092a6b082ce7f18a87
MartinGrue <gruebler.martin@outlook.de>
2020-12-17 17:11:59 +0100

    made custom timeleft hook

---
+++ tickets/renderer/pages/hooks/timeleft.ts
@@ -0,0 +1,14 @@
+import { time } from "console";
+import { useState } from "react";
+import { IOrder } from "../../app/models/Order";
+
+const useTimeLeft = (order: IOrder) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const findTimeLeft = () => {
+    const oderDate = new Date(order.expiresAt);
+    const msLeft = Math.abs(oderDate.getTime() - new Date().getTime());
+    setTimeLeft(Math.round(msLeft / 1000));
+  };
+  return { timeLeft, findTimeLeft };
+};
+export { useTimeLeft };
