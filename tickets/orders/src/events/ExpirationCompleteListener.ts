b9e53a083c7446c94450cd4f39df1ab16383063f
MartinGrue <gruebler.martin@outlook.de>
2020-12-20 13:21:20 +0100

    added test for expiration service

---
+++ tickets/orders/src/events/ExpirationCompleteListener.ts
@@ -0,0 +1,35 @@
+import nats, { Message } from "node-nats-streaming";
+import {
+  Subjects,
+  Listener,
+  ExpirationCompleteEvent,
+  OrderStatus,
+} from "@scope/common";
+import { quegrpName } from "./queueGroupName";
+import { Order } from "../models/Orders";
+import { CancelOrderPublisher } from "./CancelOrderPublisher";
+import { natsWrapper } from "../NatsWrapper";
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = quegrpName;
+  onMessage = async (data: ExpirationCompleteEvent["data"], msg: Message) => {
+    const { orderId } = data;
+
+    const order = await Order.findById(orderId).populate("ticket");
+    if (!order) {
+      throw new Error("Order not found");
+    }
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+    if (order.id && order.ticket.id) {
+      new CancelOrderPublisher(natsWrapper.client).publish({
+        id: order.id,
+        version: order.version,
+        ticket: {
+          id: order.ticket.id,
+        },
+      });
+    }
+    msg.ack();
+  };
+}
---
+++ tickets/orders/src/events/__test__/ExpirationCompleteListener.test.ts
@@ -0,0 +1,67 @@
+import { Message } from "node-nats-streaming";
+import { ExpirationCompleteEvent, OrderStatus } from "@scope/common";
+import mongoose from "mongoose";
+import { natsWrapper } from "../../NatsWrapper";
+import { ExpirationCompleteListener } from "../ExpirationCompleteListener";
+import { Ticket } from "../../models/Ticket";
+import { Order } from "../../models/Orders";
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    ticketId: new mongoose.Types.ObjectId().toHexString(),
+    title: "test",
+    price: 1,
+  });
+  await ticket.save();
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: "12323",
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id ? order.id : undefined,
+  };
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+  return { listener, order, ticket, data, msg };
+};
+it("updated the order status to cancelled", async () => {
+  const { listener, order, ticket, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+  const updatedOrder = await Order.findById(data.orderId);
+  expect(updatedOrder).toBeDefined();
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+it("emit an OrderCancelled event", async () => {
+  const { listener, order, ticket, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+  const eventData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+  expect(eventData.id).toEqual(order.id);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+  expect(msg.ack).toHaveBeenCalled();
+});
+it("throws an error when the order to be updated can not be found", async (done) => {
+  const { msg, data, listener, ticket } = await setup();
+
+  data.orderId = new mongoose.Types.ObjectId().toHexString();
+
+  try {
+    await listener.onMessage(data, msg);
+  } catch (err) {
+    return done();
+  }
+});
